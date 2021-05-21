814f8022d0639d31ee729b89f0f6d1b0291dc1d3
SylvanWoods <913545731@qq.com>
2021-05-21 09:48:40 +0800

    sentinel

---
+++ sentinel/src/main/java/com/shone/nacos/cloudconfigexample/controller/SentinelApplication.java
@@ -0,0 +1,16 @@
+package com.shone.nacos.cloudconfigexample.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SentinelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelApplication.class, args);
+    }
+
+}
---
+++ sentinel/src/main/java/com/shone/nacos/cloudconfigexample/controller/TestController.java
@@ -0,0 +1,43 @@
+package com.shone.nacos.cloudconfigexample.controller;
+
+
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.ctc.wstx.util.ExceptionUtil;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class TestController {
+
+    @GetMapping("test")
+    public String test1() {
+           return hello(3L);
+    }
+
+    // 原函数
+    @SentinelResource(value = "hello", blockHandler = "exceptionHandler", fallback = "helloFallback")
+    public String hello(long s) {
+        return String.format("Hello at %d", s);
+    }
+
+    // Fallback 函数，函数签名与原函数一致或加一个 Throwable 类型的参数.
+    public String helloFallback(long s) {
+        return String.format("Halooooo %d", s);
+    }
+
+    // Block 异常处理函数，参数最后多一个 BlockException，其余与原函数一致.
+    public String exceptionHandler(long s, BlockException ex) {
+        // Do some log here.
+        ex.printStackTrace();
+        return "Oops, error occurred at " + s;
+    }
+
+    // 这里单独演示 blockHandlerClass 的配置.
+    // 对应的 `handleException` 函数需要位于 `ExceptionUtil` 类中，并且必须为 public static 函数.
+    @SentinelResource(value = "test", blockHandler = "handleException", blockHandlerClass = {ExceptionUtil.class})
+    public void test() {
+        System.out.println("Test");
+    }
+}
